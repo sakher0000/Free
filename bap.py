99cb2ec27976e140731cf6f60583f32aec258473
SAKHER-ID <131241563+sakher0000@users.noreply.github.com>
2023-07-05 12:55:08 +0545

    Add files via upload

--- bap.py
+++ bap.py
@@ -673,13 +673,11 @@ def passwrd():
 					pwv.append(frs+'321')
 					pwv.append('@@'+frs)
 					pwv.append('@'+frs+'123')
-					pwv.append(frs+'1111')
-					pwv.append(frs+'1122')
 					pwv.append(frs+'#123')
 					pwv.append(frs+'#1234')
 					pwv.append(frs+'#12345')
-					pwv.append(frs+'@@##')
-					pwv.append(frs+'££££')
+					pwv.append(frs+'1111')
+					pwv.append(frs+'1122')
 			else:
 				if len(frs)<3:
 					pwv.append(nmf)
@@ -701,13 +699,11 @@ def passwrd():
 					pwv.append(frs+'321')
 					pwv.append('@@'+frs)
 					pwv.append('@'+frs+'123')
-					pwv.append(frs+'1111')
-					pwv.append(frs+'1122')
 					pwv.append(frs+'#123')
 					pwv.append(frs+'#1234')
 					pwv.append(frs+'#12345')
-					pwv.append(frs+'@@##')
-					pwv.append(frs+'££££')
+					pwv.append(frs+'1111')
+					pwv.append(frs+'1122')
 			if 'ya' in pwpluss:
 				for xpwd in pwnya:
 					pwv.append(xpwd)
